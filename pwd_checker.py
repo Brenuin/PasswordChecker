2e03743c57cf5d822309e645e36309b79815ff23
Speckeldore <santossk@oregonstate.edu>
2024-02-19 13:53:26 -0800

    fixed the failed tests for the passwords that didnt have a upper or lower case

--- pwd_checker.py
+++ pwd_checker.py
@@ -1,5 +1,10 @@
 def check_pwd(s):
     if len(s) < 8 or len(s) > 20:
         return False
+    if not any(char.isupper() for char in s):
+        return False
+
+    if not any(char.isdigit() for char in s):
+        return False
     return True
 
